a3dff0b4a44aac6776fcf95e87a0142d782bc7ee
dipsanstha <dipsanno1@gmail.com>
2025-09-29 00:19:19 +0545

    update

--- src/servicesData.js
+++ src/servicesData.js
@@ -5,20 +5,20 @@ export const servicesByCategory = {
         { name: 'Laxmi\'s Special', image: 'OIP.jpg', description: ' Indulgent skincare experience that combines the tightening and lifting benefits of Poly Lactic with the rejuvenating effects of elevate skin with a tightening, collagen-boosting facial for lifted, youthful radiance.', price: '$150', duration: '60 min', icon: 'fa-spa' },
         { name: 'O2 Derm Oxygen Facial', image: 'oxygenfacial.jpg', description: 'Rejuvenate your skin with the oxygen facial, perfect for hydration and revitalization.', price: '$150', duration: '65 min', icon: 'fa-spa' },
         { name: 'GlowTox Facelift Facial', image: 'glow.jpg', description: 'Lift and brighten your face with this signature treatment that promotes youthful radiance and tightens the skin.', price: '$200', duration: '70 min', icon: 'fa-spa' },
-        { name: 'Microchanneling Special', image: 'swedish-massage.jpg', description: 'Enhance your skin\'s texture with our microchanneling treatment, ideal for reducing fine lines and scars.', price: '$225', duration: '60 min', icon: 'fa-spa' },
+        { name: 'Microchanneling Special', image: 'microchanneling.jpg', description: 'Enhance your skin\'s texture with our microchanneling treatment, ideal for reducing fine lines and scars.', price: '$225', duration: '60 min', icon: 'fa-spa' },
         { name: 'Acne Facial', image: 'acne.jpg', description: 'Treat acne-prone skin with our specialized acne facial that helps to cleanse, balance, and reduce inflammation.', price: '$95', duration: '55 min', icon: 'fa-spa' },
         { name: 'Microdermabrasion', image: 'OIP4.jpg', description: 'Exfoliate and refresh your skin with this rejuvenating microdermabrasion treatment.', price: '$120', duration: '60 min', icon: 'fa-fire' }
     ],
     '2. Add-On Treatments': [
-        { name: 'Microdermabrasion', image: 'OIP4.jpg', description: 'Exfoliate and refresh your skin with this rejuvenating microdermabrasion treatment.', price: '$50', duration: '10 min', icon: 'fa-fire' },
         { name: 'LED Therapy', image: 'LED-Light-Therapy-Treatment-1080x675.png', description: 'Boost your skin\'s healing process with our LED therapy, designed to treat acne, aging, and pigmentation.', price: '$55', duration: '30 min', icon: 'fa-lightbulb' },
-        { name: 'Goat Milk Peel', image: 'goat.jpg', description: 'A soothing and nourishing peel, perfect for sensitive skin.', price: '$25', duration: '10 min', icon: 'fa-fire' }
+        { name: 'Goat Milk Peel', image: 'goat.jpg', description: 'A soothing and nourishing peel, perfect for sensitive skin.', price: '$25', duration: '10 min', icon: 'fa-fire' },
+        { name: 'Microdermabrasion', image: 'OIP4.jpg', description: 'Exfoliate and refresh your skin with this rejuvenating microdermabrasion treatment.', price: '$50', duration: '10 min', icon: 'fa-fire' }
     ],
     '3. Lash & Brow Services': [
         { name: 'Lash Lift', image: 'R.jpg', description: 'Lift and curl your lashes for a natural, wide-eyed look.', price: '$85', duration: '50 min', icon: 'fa-eye' },
         { name: 'Brow Lamination', image: 'sauna-6766761_1280.jpg', description: 'Shape and set your brows for a fuller, more defined look.', price: '$75', duration: '50 min', icon: 'fa-eye' },
         { name: 'Lash Tint', image: 'lashtint.jpg', description: 'Darken your lashes for a dramatic effect without the need for mascara.', price: '$20', duration: '20 min', icon: 'fa-eye' },
-        { name: 'Brow Tint', image: 'sauna-6766761_1280.jpg', description: 'Enhance the shape and color of your brows with a custom tinting treatment.', price: '$20', duration: '15 min', icon: 'fa-eye' },
+        { name: 'Brow Tint', image: 'browtint.jpg', description: 'Enhance the shape and color of your brows with a custom tinting treatment.', price: '$20', duration: '15 min', icon: 'fa-eye' },
         { name: 'Brow + Lash Tint Combo', image: 'brow-lash-combo.jpg', description: 'Enhance both your brows and lashes with our combined tinting treatment.', price: '$35', duration: '30 min', icon: 'fa-eye' }
     ],
     '4. Body Treatments': [
@@ -27,7 +27,7 @@ export const servicesByCategory = {
         { name: 'Hyaluronic Acid Infusion', image: 'hyaluronic.jpg', description: 'Hydrate and plump your skin with our hyaluronic acid infusion treatment.', price: '$120', duration: '60 min', icon: 'fa-spa' }
     ],
     '5. Waxing Services - Face': [
-        { name: 'Eyebrow Wax', image: 'liwax.jpg', description: 'Shape and define your brows with our precise waxing service.', price: '$25', duration: '15 min', icon: 'fa-bullseye' },
+        { name: 'Eyebrow Wax', image: 'eyebrow.jpg', description: 'Shape and define your brows with our precise waxing service.', price: '$25', duration: '15 min', icon: 'fa-bullseye' },
         { name: 'Lip Wax', image: 'liwax.jpg', description: 'Quick and gentle waxing for a smooth and clean upper lip.', price: '$10', duration: '10 min', icon: 'fa-bullseye' },
         { name: 'Chin Wax', image: 'chin.jpg', description: 'Quick and precise chin waxing for smooth, hair-free skin.', price: '$10', duration: '15 min', icon: 'fa-location-arrow' },
         { name: 'Full Face Wax', image: 'Facial-Waxing-e1420480171351-1024x920.jpg', description: 'Remove unwanted hair from your entire face for smooth, flawless skin.', price: '$55', duration: '40 min', icon: 'fa-bookmark' },
@@ -39,8 +39,8 @@ export const servicesByCategory = {
         { name: 'Underarm Wax', image: 'd019b8c020ccec881b14fd77bd91b992.jpg', description: 'Get smooth underarms with our quick and effective waxing treatment.', price: '$25', duration: '15 min', icon: 'fa-bookmark' },
         { name: 'Half Legs Wax', image: 'leg.jpg', description: 'Remove hair from your knees down to your ankles for smooth legs.', price: '$50', duration: '35 min', icon: 'fa-map' },
         { name: 'Full Legs Wax', image: 'legg.jpg', description: 'Get smooth, hair-free legs from top to bottom.', price: '$90', duration: '55 min', icon: 'fa-map' },
-        { name: 'Half Arms Wax', image: 'sauna-6766761_1280.jpg', description: 'Get smooth arms from your elbows down to your wrists.', price: '$35', duration: '30 min', icon: 'fa-map' },
-        { name: 'Full Arms Wax', image: 'arm.jpg', description: 'Wax your full arms from wrist to shoulder for smooth skin.', price: '$60', duration: '40 min', icon: 'fa-map' }
+        { name: 'Half Arms Wax', image: 'fullarm.jpg', description: 'Get smooth arms from your elbows down to your wrists.', price: '$35', duration: '30 min', icon: 'fa-map' },
+        { name: 'Full Arms Wax', image: 'fullhand.jpg', description: 'Wax your full arms from wrist to shoulder for smooth skin.', price: '$60', duration: '40 min', icon: 'fa-map' }
     ],
     '7. Threading Services': [
         { name: 'Brow Threading', image: 'thre.jpg', description: 'Shape your brows with precision threading for a flawless look.', price: '$25', duration: '20 min', icon: 'fa-map' },
